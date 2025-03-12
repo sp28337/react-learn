1e691b20981e17ed2d227f08332aeecaf9de3edb
Pavel Tarakanov <pavel191094@icloud.com>
2025-03-12 15:54:07 +0300

    add HeaderContainer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React from "react"
+import Main from "./components/pages/Main"
 
 function App() {
   return (
     <React.Fragment>
-
+      <Main></Main>
     </React.Fragment>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/pages/Main.jsx
+++ src/components/pages/Main.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React from "react"
+import Head from "../views/global/Head"
 
 
 const Main = () => {
     return (
         <React.Fragment>
+            <Head></Head>
         </React.Fragment>        
     )
 }

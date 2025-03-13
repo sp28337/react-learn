286e7d2535e9113abb60cc3eede2717b8047433f
Pavel Tarakanov <pavel191094@icloud.com>
2025-03-13 09:42:41 +0300

    add FormContainer component

--- src/components/pages/Main.jsx
+++ src/components/pages/Main.jsx
@@ -1,12 +1,15 @@
 import React from "react"
 import Head from "../views/global/Head"
 import Foot from "../views/global/Foot"
+import css from "../../styles/form"
 
+const { FormContainer } = css
 
 const Main = () => {
     return (
         <React.Fragment>
             <Head></Head>
+            <FormContainer></FormContainer>
             <Foot></Foot>
         </React.Fragment>        
     )
---
+++ src/styles/form.js
@@ -0,0 +1,19 @@
+import styled from "styled-components"
+
+const css = {
+    FormContainer: styled.div`
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: flex-start;
+        position: relative;
+        width: 600px;
+        height: auto;
+        min-height: 60px;
+        border-radisus: 8px;
+        box-shadow: 0px 0px 3px grey;
+        margin: 40px auto;
+    `,
+}
+
+export default css

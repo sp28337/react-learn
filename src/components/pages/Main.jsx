1feea848a98edac287881fdec4ccbfbdda71a29b
Pavel Tarakanov <pavel191094@icloud.com>
2025-03-13 15:28:44 +0300

    add validation func to change background-color of Button component

--- src/components/pages/Main.jsx
+++ src/components/pages/Main.jsx
@@ -8,9 +8,15 @@ const { FormContainer, Button } = css
 
 const Main = () => {
 
-    const [ value, setValue ] = useState()
-    const [ type, setType ] = useState()
-    const [ comment, setComment ] = useState()
+    const [ value, setValue ] = useState("")
+    const [ type, setType ] = useState("")
+    const [ comment, setComment ] = useState("")
+
+    const validation = () => {
+        if (value.length > 2 && type) {
+            console.log("Successful validation")
+        } else console.log("Failed validation")
+    }
 
     return (
         <React.Fragment>
@@ -19,11 +25,11 @@ const Main = () => {
                 <InputComponent action={setValue} placeholder={"Transaction type"} maxLength={"100"}/>
                 <InputComponent action={setType} placeholder={"Transaction sum"} maxLength={"100"}/>
                 <InputComponent action={setComment} placeholder={"Enter comment"} maxLength={"100"}/>
-                <Button backgroundColor={"#B0F347"}>Save transaction</Button>
+                <Button 
+                    backgroundColor={
+                        value.length < 3 ? "#E5E5E5" : 
+                        type ? "#B0F347" : "#E5E5E5"} onClick={validation}>Save transaction</Button>
             </FormContainer>
-            <span>{value}</span><br/>
-            <span>{type}</span><br/>
-            <span>{comment}</span>
             <Foot></Foot>
         </React.Fragment>        
     )

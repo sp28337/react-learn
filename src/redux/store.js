202f42324a673a427c6c2b5200b08e7d798a8c73
Pavel Tarakanov <pavel191094@icloud.com>
2025-03-21 11:00:48 +0300

    add mainDataSlice reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterSlice from './reducers/counter'
+import mainDataSlice from './reducers/main-data-reducer'
 
 export const store = configureStore({
   reducer: {
     counter: counterSlice,
+    typeReducer: mainDataSlice,
   },
 })
